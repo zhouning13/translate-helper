22b87a2a228da696b4a09f6fc4c86441d98e8dc7
冷凝沙漠 <zhouning13@126.com>
2018-09-03 16:52:47 +0800

    更新地图事件和公共事件的提取方式：不再获取所有内容，只处理 401 文本、102 选择项、402 被选择的判断。

--- src/main/java/club/gorpg/helper/service/impl/json/CommonEventFileService.java
+++ src/main/java/club/gorpg/helper/service/impl/json/CommonEventFileService.java
@@ -45,7 +45,9 @@ public class CommonEventFileService implements IJsonFileService {
 			}
 
 			if (n != null && !n.isNull()) {
-				fm.add("$[" + i + "].name", new SectionInfo(n.get("name").textValue(), "公共事件" + i));
+				// fm.add("$[" + i + "].name", new SectionInfo(n.get("name").textValue(), "公共事件"
+				// + i));
+				String eventName = n.get("name").textValue();// 事件名称
 
 				JsonNode list = n.get("list");
 				if (list == null || list.isNull()) {
@@ -53,19 +55,40 @@ public class CommonEventFileService implements IJsonFileService {
 				}
 				int j = 0;
 				for (JsonNode node : list) {
-					if (node != null && !node.isNull()) {
-						JsonNode param = node.get("parameters");
-						if (param == null || param.isNull()) {
-							continue;
-						}
-						int k = 0;
-						for (JsonNode p : param) {
-							if (p != null && !p.isNull() && p.isTextual()) {
-								fm.add("$[" + i + "].list[" + j + "].parameters[" + k + "]",
-										new SectionInfo(p.textValue(), "公共事件" + i + "的" + j + "组" + k + "号参数"));
+
+					int code = node.get("code").asInt();
+
+					if (code == 401) {
+						// 401 文本类
+						JsonNode text = node.get("parameters").get(0);
+						fm.add("$.events[" + i + "].list[" + j + "].parameters[0]",
+								new SectionInfo(text.asText(), "公共事件" + i + "“" + eventName + "”的" + j + "行的文本"));
+					}
+
+					if (code == 102) {
+						// 102 选项
+						JsonNode params = node.get("parameters").get(0);
+						if (params != null && !params.isNull()) {
+							int m = 0;
+							for (JsonNode param : params) {
+								if (param != null && !param.isNull()) {
+									fm.add("$.events[" + i + "].list[" + j + "].parameters[0][" + m + "]",
+											new SectionInfo(param.textValue(),
+													"公共事件" + i + "“" + eventName + "”的" + j + "行选择的第" + m + "个选项"));
+								}
+								m++;
 							}
-							k++;
 						}
+
+					}
+
+					if (code == 402) {
+						// 402 选项被选择
+						JsonNode params = node.get("parameters");
+						JsonNode index = params.get(0);
+						JsonNode text = params.get(1);
+						fm.add("$.events[" + i + "].list[" + j + "].parameters[1]", new SectionInfo(text.textValue(),
+								"事件" + i + "“" + eventName + "”的" + j + "行选择的第" + index + "个选项，需与前面的选项翻译一致！"));
 					}
 					j++;
 				}
--- src/main/java/club/gorpg/helper/service/impl/json/MapFileService.java
+++ src/main/java/club/gorpg/helper/service/impl/json/MapFileService.java
@@ -34,31 +34,62 @@ public class MapFileService implements IJsonFileService {
 			int i = 0;
 			for (JsonNode event : events) {
 				if (event != null && !event.isNull()) {
-					fm.add("$.events[" + i + "].name", new SectionInfo(event.get("name").textValue(), "事件" + i));
+					// fm.add("$.events[" + i + "].name", new SectionInfo(, "事件" + i));
+					String eventName = event.get("name").textValue();// 事件名称
 
 					JsonNode pages = event.get("pages");
 					if (pages != null && !pages.isNull()) {
 						int j = 0;
 						for (JsonNode p : pages) {
 							if (p != null && !p.isNull()) {
+
 								JsonNode list = p.get("list");
 								if (list != null && !list.isNull()) {
 									int k = 0;
 									for (JsonNode l : list) {
 										if (l != null && !l.isNull()) {
-											JsonNode params = l.get("parameters");
-											if (params != null && !params.isNull()) {
-												int m = 0;
-												for (JsonNode param : params) {
-													if (param != null && !param.isNull()) {
-														fm.add("$.events[" + i + "].pages[" + j + "].list[" + k
-																+ "].parameters[" + m + "]",
-																new SectionInfo(event.get("name").textValue(), "事件" + i
-																		+ "的" + j + "组" + k + "号因素的" + m + "号参数"));
-													}
-													m++;
-												}
+											int code = l.get("code").asInt();
+
+											if (code == 401) {
+												// 401 文本类
+												JsonNode text = l.get("parameters").get(0);
+												fm.add("$.events[" + i + "].pages[" + j + "].list[" + k
+														+ "].parameters[0]",
+														new SectionInfo(text.asText(), "事件" + i + "“" + eventName + "”的"
+																+ j + "页" + k + "行的文本"));
 											}
+
+											if (code == 102) {
+												// 102 选项
+												JsonNode params = l.get("parameters").get(0);
+												if (params != null && !params.isNull()) {
+													int m = 0;
+													for (JsonNode param : params) {
+														if (param != null && !param.isNull()) {
+															fm.add("$.events[" + i + "].pages[" + j + "].list[" + k
+																	+ "].parameters[0][" + m + "]",
+																	new SectionInfo(param.textValue(),
+																			"事件" + i + "“" + eventName + "”的" + j + "页"
+																					+ k + "行选择的第" + m + "个选项"));
+														}
+														m++;
+													}
+												}
+
+											}
+
+											if (code == 402) {
+												// 402 选项被选择
+												JsonNode params = l.get("parameters");
+												JsonNode index = params.get(0);
+												JsonNode text = params.get(1);
+												fm.add("$.events[" + i + "].pages[" + j + "].list[" + k
+														+ "].parameters[1]",
+														new SectionInfo(text.textValue(),
+																"事件" + i + "“" + eventName + "”的" + j + "页" + k
+																		+ "行选择的第" + index + "个选项，需与前面的选项翻译一致！"));
+											}
+
 										}
 										k++;
 									}
